ff3526ad31ca1488b7dcb9948725a8a2b650db8d
amreshkumar-web <121627475+amreshkumar-web@users.noreply.github.com>
2023-01-18 13:40:47 +0530

    added index file

---
+++ webjava.js
@@ -0,0 +1,14 @@
+
+var show=document.getElementById("HIDE");
+var hide=document.getElementById("fhide");
+var info=document.getElementById("ginfo2")
+function change(){
+    hide.className=("fhide2");
+    show.className="HIDE2";
+    info.className="ginfo";
+}
+function back(){
+    show.className="HIDE";
+    hide.className="fhide";
+    info.className="ginfo2";
+}
